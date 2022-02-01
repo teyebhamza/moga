c2a099955c910c7108e5e59d44f0cda272ae58bc
Hamza TEYEB <teyeb.hamza12@gmail.com>
2022-02-01 16:22:03 +0100

    add js files

---
+++ challenge-web-nft/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './componnet/Header'
+
+function App() {
+  return (
+  <div className='header'>
+  <Header />
+  </div>
+  )
+}
+
+export default App;

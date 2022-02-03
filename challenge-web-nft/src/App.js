a06b3ac4d10ff9591d1dc2c4d07a89c10b50d4fd
Hamza TEYEB <teyeb.hamza12@gmail.com>
2022-02-04 00:52:44 +0100

    add axios application

--- challenge-web-nft/src/App.js
+++ challenge-web-nft/src/App.js
@@ -1,27 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './Componnet/Header';
-import CollectionCard from './Componnet/CollectionCard';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import logo from './logo.svg'
+import './App.css'
+import Header from './Componnet/Header'
+import CollectionCard from './Componnet/CollectionCard'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
   const [mogaListData, setMogaListData] = useState([])
 
-  useEffect(()  => {
+  useEffect(() => {
     const getMyNfts = async () => {
-      const openseaData = await axios.get(
-      'https://thirdweb.com/rinkeby/0x9501bbAebb2f0aF4233f2Fe38EA0ad7140b09461/nft-collection/0x730423Ecf23A7a2C03DE96830df14BD7a1F691cb'
-      )
-      console.log(openseaData.data.assets)
+    const openseaData = await axios.get(
+      'https://testnets-api.opensea.io/assets?asset_contract_adress=0x730423ecf23a7a2c03de96830df14bd7a1f691cb&order_direction=asc'
+    )
+    console.log(openseaData.data.assets)
     }
     return getMyNfts()
-  }, []);
+  }, [])
+
+  
 
   return (
   <div className='app'>
   <Header />
-  <CollectionCard  id={0} name={'Bebars Game'} traits={[{'value': 2}]} image='https://lh3.googleusercontent.com/WOe0tE6yirvGltLk89jh2OumW4DKf1rWMSjPohYOMcg2QgfvbDupxdWEu-4cNZkgJk7XXvfTTAzexCnbR8ANTbh26Q8fHl9KTxb8uL8=w600'/>
+  <CollectionCard  id={0} name={'Bebars Game'} traits={[{'value': 2}]} image='https://lh3.googleusercontent.com/RF8LL60iCIM3QguKHVn4vaFzeG2_hn-NNigXbxQ78QXbgJCnR8PS-nDCD7PS73AEL3miz2adaSYxHF0f8GhvRzhBdFIkyZF9gnzL=w600'/>
   </div>
   )
 }
